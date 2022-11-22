011e0d8c25099d922d1a45fc1b7b766820ea32a5
Majster na beacie ziomal, latam na dronie jak na tureckim dywanie, pytasz czy jaram? <majster247.bezdowozu@kxh-research.gov>
2022-11-22 11:29:36 +0100

    Dzialanie buttonu sortowania (do poprawy)

--- app/src/main/java/com/example/zadanie22112022/MainActivity.kt
+++ app/src/main/java/com/example/zadanie22112022/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.zadanie22112022
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Switch
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
@@ -33,6 +34,83 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.btn_sort).setOnClickListener {
 
+            if (RandomList.size != 0)
+            {
+                val RandomList_sorted = ArrayList<Int>()
+
+                val s1 = findViewById<Switch>(R.id.switch1)
+                val s2 = findViewById<Switch>(R.id.switch2)
+                val s3 = findViewById<Switch>(R.id.switch3)
+                val s4 = findViewById<Switch>(R.id.switch4)
+                val s5 = findViewById<Switch>(R.id.switch5)
+                val s6 = findViewById<Switch>(R.id.switch6)
+                val s7 = findViewById<Switch>(R.id.switch7)
+                val s8 = findViewById<Switch>(R.id.switch8)
+                val s9 = findViewById<Switch>(R.id.switch9)
+
+                if (s1.isChecked) {
+                    RandomList_sorted.add(s1.text.toString().toInt())
+                }
+
+                if (s2.isChecked) {
+                    RandomList_sorted.add(s2.text.toString().toInt())
+                }
+
+                if (s3.isChecked) {
+                    RandomList_sorted.add(s3.text.toString().toInt())
+                }
+
+                if (s4.isChecked) {
+                    RandomList_sorted.add(s4.text.toString().toInt())
+                }
+
+                if (s5.isChecked) {
+                    RandomList_sorted.add(s5.text.toString().toInt())
+                }
+
+                if (s6.isChecked) {
+                    RandomList_sorted.add(s6.text.toString().toInt())
+                }
+
+                if (s7.isChecked) {
+                    RandomList_sorted.add(s7.text.toString().toInt())
+                }
+
+                if (s8.isChecked) {
+                    RandomList_sorted.add(s8.text.toString().toInt())
+                }
+
+                if (s9.isChecked) {
+                    RandomList_sorted.add(s9.text.toString().toInt())
+                }
+
+                val dlugosc = RandomList_sorted.size
+                val min_dlugosc = dlugosc - 1
+
+               for (j in 0..min_dlugosc)
+               {
+                   val jj = j+1
+
+                   val current = RandomList_sorted[j].toString().toInt()
+                   val next = RandomList_sorted[jj].toString().toInt() // NIE DZIALA
+
+                    if (current > next)
+                    {
+                        RandomList_sorted[j] = next
+                        RandomList_sorted[j+1] = current
+                    }
+               }
+
+                if (RandomList_sorted.size == 0)
+                {
+                    findViewById<TextView>(R.id.textView_result).text = "Brak elementów do posortowania"
+                }
+
+                else
+                {
+                    findViewById<TextView>(R.id.textView_result).text = RandomList_sorted.toString()
+                }
+            }
         }
     }
 }
