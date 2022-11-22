0f54d5447f1f958b7e5decb0aa32e6e331e73049
Majster na beacie ziomal, latam na dronie jak na tureckim dywanie, pytasz czy jaram? <majster247.bezdowozu@kxh-research.gov>
2022-11-22 10:45:48 +0100

    Dzialanie buttonu losuj

--- app/src/main/java/com/example/zadanie22112022/MainActivity.kt
+++ app/src/main/java/com/example/zadanie22112022/MainActivity.kt
@@ -2,19 +2,33 @@ package com.example.zadanie22112022
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val RandomList = ArrayList<Int>()
+
+        for (i in 0..8)
+        {
+            RandomList.add(Random.nextInt(0, 100))
+        }
+
         findViewById<Button>(R.id.btn_random).setOnClickListener {
 
-            for (i in 1..9)
-            {
-
-            }
+            findViewById<Switch>(R.id.switch1).text = RandomList[0].toString()
+            findViewById<Switch>(R.id.switch2).text = RandomList[1].toString()
+            findViewById<Switch>(R.id.switch3).text = RandomList[2].toString()
+            findViewById<Switch>(R.id.switch4).text = RandomList[3].toString()
+            findViewById<Switch>(R.id.switch5).text = RandomList[4].toString()
+            findViewById<Switch>(R.id.switch6).text = RandomList[5].toString()
+            findViewById<Switch>(R.id.switch7).text = RandomList[6].toString()
+            findViewById<Switch>(R.id.switch8).text = RandomList[7].toString()
+            findViewById<Switch>(R.id.switch9).text = RandomList[8].toString()
         }
 
         findViewById<Button>(R.id.btn_sort).setOnClickListener {
